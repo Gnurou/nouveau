9d21a02ba7597c077a43b7b778bbf8918830adf6
Alexandre Courbot <acourbot@nvidia.com>
2015-09-02 15:01:39 +0900

    fifo/gm20b: kick channel during cleanup
    GM20B requires a channel kick to be performed during gpfifo cleanup, or
    the FIFO will attempt to fetch memory from the previous context as a
    channel is recycled. A previous commit attempted to do this for all
    Kepler GPUs, but due to bug reports that pinned it down it has been
    reverted. The present commit limits its scope to GM20B only.
    
    The only effective change of this patch is to add a call to
    gk104_fifo_gpfifo_kick() in gpfifo_fini for GM20B, but doing so requires
    to export quite a few extra functions, hence its non-trivial length.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- drm/nouveau/nvkm/engine/fifo/changk104.h
+++ drm/nouveau/nvkm/engine/fifo/changk104.h
@@ -21,9 +21,26 @@ struct gk104_fifo_chan {
 	} engn[NVKM_SUBDEV_NR];
 };
 
+int gk104_fifo_gpfifo_kick(struct gk104_fifo_chan *);
+void *gk104_fifo_gpfifo_dtor(struct nvkm_fifo_chan *);
+void gk104_fifo_gpfifo_init(struct nvkm_fifo_chan *);
+int gk104_fifo_gpfifo_engine_ctor(struct nvkm_fifo_chan *, struct nvkm_engine *,
+				  struct nvkm_object *);
+void gk104_fifo_gpfifo_engine_dtor(struct nvkm_fifo_chan *,
+				   struct nvkm_engine *);
+int gk104_fifo_gpfifo_engine_init(struct nvkm_fifo_chan *,
+				  struct nvkm_engine *);
+int gk104_fifo_gpfifo_engine_fini(struct nvkm_fifo_chan *, struct nvkm_engine *,
+				  bool);
+
+int __gk104_fifo_gpfifo_new(struct nvkm_fifo *, const struct nvkm_oclass *,
+			    const struct nvkm_fifo_chan_func *, void *, u32,
+			    struct nvkm_object **);
+
 int gk104_fifo_gpfifo_new(struct nvkm_fifo *, const struct nvkm_oclass *,
 			  void *data, u32 size, struct nvkm_object **);
 
 extern const struct nvkm_fifo_chan_oclass gk104_fifo_gpfifo_oclass;
 extern const struct nvkm_fifo_chan_oclass gm204_fifo_gpfifo_oclass;
+extern const struct nvkm_fifo_chan_oclass gm20b_fifo_gpfifo_oclass;
 #endif
